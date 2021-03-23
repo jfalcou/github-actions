5478946ae0c14248dd5edafa1611bc8bd6d59a1a
Joel Falcou <joel.falcou@lri.fr>
2021-03-23 20:47:35 +0100

    Report failures

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 compile_target()
 {
   echo "::group::Compiling $1" ;
-  ninja $1 -j $2 -k 0;
+  ninja $1 -j $2;
   compile=$?;
   echo "::endgroup::" ;
 
@@ -18,7 +18,7 @@ compile_targets()
   if [ -z $? ]
   then
     echo "::error $i can not be compiled!" ;
-    exit -1 ;
+    return 1 ;
   fi
   done;
 }
@@ -41,7 +41,7 @@ test_targets()
     if [ -z $? ]
     then
       echo "::error $i tests failed!" ;
-      exit -1 ;
+      return 1 ;
     fi
   done;
 }
@@ -59,13 +59,49 @@ cmake .. -G Ninja -DCMAKE_CXX_FLAGS="$3" $4
 echo "::endgroup::"
 
 compile_targets ../cmake/toolchain/arch.targets.json
-test_targets ../cmake/toolchain/arch.targets.json
+if [ -z $? ]
+then
+  return -1 ;
+fi
+
+test_targets    ../cmake/toolchain/arch.targets.json
+if [ -z $? ]
+then
+  return -1 ;
+fi
 
 compile_targets ../cmake/toolchain/api.targets.json
-test_targets ../cmake/toolchain/api.targets.json
+if [ -z $? ]
+then
+  return -1 ;
+fi
+
+test_targets    ../cmake/toolchain/api.targets.json
+if [ -z $? ]
+then
+  return -1 ;
+fi
 
 compile_targets ../cmake/toolchain/doc.targets.json
-test_targets ../cmake/toolchain/doc.targets.json
+if [ -z $? ]
+then
+  return -1 ;
+fi
+
+test_targets    ../cmake/toolchain/doc.targets.json
+if [ -z $? ]
+then
+  return -1 ;
+fi
 
 compile_targets ../cmake/toolchain/simd.targets.json
-test_targets ../cmake/toolchain/simd.targets.json
+if [ -z $? ]
+then
+  return -1 ;
+fi
+
+test_targets    ../cmake/toolchain/simd.targets.json
+if [ -z $? ]
+then
+  return -1 ;
+fi
